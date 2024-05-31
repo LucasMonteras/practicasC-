9e6bdb45fa2ce8c9ea68a54ab0b6cde2f052ae6a
LucasMonteras <lucasmonteras@gmail.com>
2024-05-31 17:47:20 -0300

    se realizo ejercicio de comentarios y buenas practicas

---
+++ desafioComentarios/Program.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+
+
+Este código invierte un mensaje, cuenta el número de veces 
+Aparece un carácter en particular y luego imprime los resultados.
+a la ventana de la consola..
+
+*/
+
+
+string mensaje = "El veloz zorro marrón salta sobre el perro perezoso.";
+
+char[] charMessage = mensaje.ToCharArray();
+Array.Reverse(charMessage);
+
+int letterCount = 0;
+
+foreach (char i in charMessage) 
+{ if (i == 'o') 
+    { 
+        letterCount++; 
+    } 
+}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' aparece {letterCount} veces.");
+
+
+
+
+
+
+
+
+
+
+
