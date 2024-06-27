67cad291fb1d34da3182ae00c4dc871539eb2513
LucasMonteras <lucasmonteras@gmail.com>
2024-06-27 00:20:39 -0300

    se realizo variablesEnBloqueCodigo

---
+++ variablesBloqueCodigo/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+/* bool flag = true;
+int value = 0;
+if (flag)
+{
+    
+    Console.WriteLine($"Dentro del bloque de código: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Dentro del bloque de código: {value}");
+
+ */
+
+
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
+
+
+
